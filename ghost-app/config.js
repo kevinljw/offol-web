9425f894e999cb413fd84373d548f2884e617ee3
KevinLJW <evin92@gmail.com>
2016-02-09 01:03:33 +0800

    add path

--- ghost-app/config.js
+++ ghost-app/config.js
@@ -33,6 +33,9 @@ config = {
         server: {
             host: '127.0.0.1',
             port: '3000'
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
         }
     },
 
